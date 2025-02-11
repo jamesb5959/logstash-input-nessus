3d2e65d784276bae97a83d9bc8e17d296566ffb9
kmacosta2 <kmacosta2@miners.utep.edu>
2025-02-11 11:20:56 -0500

    latest version of HTTP based input plugin

--- spec/inputs/nessus_spec.rb
+++ spec/inputs/nessus_spec.rb
@@ -1,12 +1,15 @@
-# encoding: utf-8
-require 'logstash/devutils/rspec/shared_examples'
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/inputs/nessus"
 
 describe LogStash::Inputs::Nessus do
+  let(:config) { { "port" => 5055 } }
 
-  it_behaves_like "an interruptible input plugin" do
-    let(:config) { { "interval" => 100 } }
+  it "should receive HTTP POST requests and process Nessus files" do
+    plugin = LogStash::Inputs::Nessus.new(config)
+    plugin.register
+
+    # Simulate receiving a .nessus file
+    sample_event = plugin.receive("<NessusClientData_v2><Report></Report></NessusClientData_v2>")
+    expect(sample_event).not_to be_nil
   end
-
 end
